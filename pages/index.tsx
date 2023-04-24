72d4c8445039ff4d0c753a7ea965536e582d2e5c
Fan Xing <xingfan77@hotmail.com>
2023-04-23 21:59:24 -0400

    update the mobile prototype of nymasterminds project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,223 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {useState} from 'react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+export default Home;
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Home(){
+    const [menu, setMenu] = useState(<ul></ul>);
+    const [section,setSection] = useState(<h4>Recent Updates</h4>);
+    const [submenu,setSubmenu] = useState(<ul className='navigation'></ul>);
+    const [content,setContent] = useState(<section></section>);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    return (
+    <div>
+      <header>
+        <nav onClick={()=>setMenu(
+            <ul className='main-menu-container'>
+              <li className='main-menu' onClick={()=>setSection(
+                  <h4 className='submenu-title' onClick={()=>setSubmenu(
+                      <ul className='submenu-container'>
+                          <h4 onClick={()=>setSubmenu(<p></p>)}>General Information</h4>
+                          <li className='submenu' onClick={()=>setContent(
+                              <table>
+                                  <th>Region1</th>
+                                  <td>timeline</td>
+                                  <td>registration information</td>
+                                  <td>regular season forms
+                                      <button>Student downloads</button>
+                                      <button>Coach downloads</button>
+                                  </td>
+                                  <p>&nbsp;</p>
+                                  <th>Region 2</th>
+                                  <td>timeline</td>
+                                  <td>registration information</td>
+                                  <td>regular season forms</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region3</th>
+                                  <td>timeline</td>
+                                  <td>registration information</td>
+                                  <td>regular season forms</td>
+                                  <p>&nbsp;</p>
+                                  <th>State</th>
+                                  <td>playoff</td>
+                                  <td>all-stars</td>
+                              </table>)}>Registration</li>
+                          <li className='submenu' onClick={()=>setContent(<article>
+                              <p>Title</p>
+                              <p>Content</p>
+                          </article>)}>Logistics</li>
+                          <li className='submenu'>Testimonial</li>
+                      </ul>
+                      )}>General Information</h4>
+              )}>General Information</li>
+              <li className='main-menu' onClick={()=>setSection(
+                  <h4 className='submenu-title' onClick={()=>setSubmenu(
+                      <ul className='submenu-container'>
+                          <h4 onClick={()=>setSubmenu(<p></p>)}>Program Information</h4>
+                          <li className='submenu' onClick={()=>setContent(
+                              <ul>
+                                  <li onClick={()=>setContent(
+                                      <table>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                      </table>
+                                  )}>2022-2023</li>
+                                  <li onClick={()=>setContent(
+                                      <table>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                      </table>
+                                  )}>2021-2022</li>
+                                  <li onClick={()=>setContent(
+                                      <table>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                          <th>time and region</th>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <td>star --- score</td>
+                                          <p>&nbsp;</p>
+                                      </table>
+                                  )}>2020-2021</li>
+                              </ul>
+                          )}>All-Stars</li>
+                          <li className='submenu' onClick={()=>setContent(
+                              <table>
+                                  <th>State Tournament</th>
+                                  <td></td>
+                              </table>
+                          )}>State Tournament</li>
+                      </ul>
+                      )}>Program Information</h4>
+              )}>Program Information</li>
+              <li className='main-menu' onClick={()=>setSection(
+                  <h4 className='submenu-title' onClick={()=>setSubmenu(
+                      <ul className='submenu-container'>
+                          <h4 onClick={()=>setSubmenu(<p></p>)}>League Information</h4>
+                          <li className='submenu' onClick={()=>setContent(
+                              <table>
+                                  <th>Region1</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region2</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region3</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                              </table>
+                          )}>2022-23</li>
+                          <li className='submenu'  onClick={()=>setContent(
+                              <table>
+                                  <th>Region1</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region2</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region3</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                              </table>
+                          )}>2021-22</li>
+                          <li className='submenu'  onClick={()=>setContent(
+                              <table>
+                                  <th>Region1</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region2</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region3</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                              </table>
+                          )}>2020-21</li>
+                          <li className='submenu' onClick={()=>setContent(
+                              <table>
+                                  <th>Region1</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region2</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                                  <th>Region3</th>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <td>school --- score</td>
+                                  <p>&nbsp;</p>
+                              </table>
+                          )}>2019-20</li>
+                      </ul>
+                      )}>League Information</h4>
+              )}>League Information</li>
+            </ul>)}>
+            {menu}
+        </nav>
+          <Image src='/hamburger-menu@1x.png' alt='menu' className='hamburger-icon' width={50} height={50} onClick={()=>setMenu(<ul><Image src='/hamburger-menu@1x.png' alt='menu' className='hamburger-icon' width={50} height={50}/></ul>)}/>
+          <Image src='/logo.png' alt='menu' className='logo' width={96} height={96} onClick={()=>setSection(<h4>Recent Updates</h4>)}/>
+        {section}
+        {submenu}
+      </header>
+      <main onClick={()=>setMenu(<ul><Image src='/hamburger-menu@1x.png' className='hamburger-icon' alt='menu' width={50} height={50}/></ul>)}>
+          {content}
+      </main>
+      <footer>
+        <h4>Contacts</h4>
+        <p>email:nymasterminds@gmail.com</p>
+        <p>phone number: 585-473-0864</p>
+      </footer>
+    </div>
   )
 }
